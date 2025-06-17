2bf61c457409ec390fae50923e62410eea2360d1
vadikbee <65399822+vadikbee@users.noreply.github.com>
2025-06-17 22:00:28 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\GameController;
 
 #Route::get('/game/start', [GameController::class, 'start'])->name('game.start');
 #Route::post('/game/callback', [GameController::class, 'callback'])->name('game.callback');
+#teeest

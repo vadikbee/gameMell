76a92de4ccbb68c7022f75ce20d1dd2f906212ff
vadikbee <65399822+vadikbee@users.noreply.github.com>
2025-06-20 23:59:58 +0300

    олная замена проекта

--- app/Services/Http/Controllers/GameController.php
+++
@@ -1,7 +0,0 @@
-<?php
-# Контроллер для игровой логики
-namespace App\Http\Controllers;
-
-use App\Services\AtmosferaService;
-use Illuminate\Http\Request;
-
--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\GameController;
-
-#Route::get('/game/start', [GameController::class, 'start'])->name('game.start');
-#Route::post('/game/callback', [GameController::class, 'callback'])->name('game.callback');
